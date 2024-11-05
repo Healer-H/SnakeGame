804e685e6f23419f6001568818936e81458b6387
Minh Hieu Huynh <22520434@gm.uit.edu.vn>
2024-11-06 01:43:47 +0700

    construct project

---
+++ src/Game.h
@@ -0,0 +1,26 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SFML/Graphics.hpp>
+#include "Snake.h"
+#include "Food.h"
+#include "UI.h"
+
+class Game {
+public:
+    Game();
+    void run();  // Chạy vòng lặp chính của trò chơi
+
+private:
+    sf::RenderWindow window;
+    Snake snake;
+    Food food;
+    UI ui;
+    int score;
+
+    void processInput();  // Xử lý đầu vào của người chơi
+    void update();        // Cập nhật trạng thái trò chơi
+    void render();        // Vẽ các thành phần lên cửa sổ
+};
+
+#endif
---
+++ src/Snake.h
@@ -0,0 +1,20 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+#include <SFML/Graphics.hpp>
+#include <deque>
+
+class Snake {
+public:
+    Snake();
+    void move();                   // Di chuyển rắn
+    void grow();                   // Tăng chiều dài rắn khi ăn thức ăn
+    void draw(sf::RenderWindow &window);  // Vẽ rắn lên cửa sổ
+    bool checkCollision();         // Kiểm tra va chạm với chính thân mình hoặc biên
+
+private:
+    std::deque<sf::Vector2f> body;  // Vị trí của các đoạn thân rắn
+    sf::Vector2f direction;         // Hướng di chuyển
+};
+
+#endif
