731b8be9f5c4840bfb1252aae8c70f1358eaf9df
Huong <uongtradaokhong@gmail.com>
2024-11-15 19:31:24 +0700

    feat:  Food Constructor

--- src/Food.cpp
+++ src/Food.cpp
@@ -1,5 +1,15 @@
 #include "Food.h"
 #include <SFML/Graphics.hpp>
+#include <cstdlib>  
+#include <ctime>
+
+Food::Food() {
+    // Khởi tạo shape food
+    shape.setSize(sf::Vector2f(10, 10));
+    shape.setFillColor(sf::Color::Red);
+    // Seed để sinh ra số ngẫu nhiên
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+}
 
 void Food::draw(sf::RenderWindow& window) const {
     window.draw(foodShape);
