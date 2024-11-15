18f585d583ff9de7b6b10a56942a5f32fa385820
Huong <uongtradaokhong@gmail.com>
2024-11-15 19:52:54 +0700

    feat: Food::getPosition() - Lấy vị trí thức ăn

--- src/Food.cpp
+++ src/Food.cpp
@@ -1,6 +1,11 @@
 #include "Food.h"
 #include <SFML/Graphics.hpp>
 
+
+sf::Vector2f Food::getPosition() const {
+    return shape.getPosition();
+}
+
 void Food::draw(sf::RenderWindow& window) const {
     window.draw(foodShape);
 }
