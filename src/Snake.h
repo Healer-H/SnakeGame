6faa382166aed0a74f7b8cfd9a327a77f4eed853
Minh Hieu Huynh <22520434@gm.uit.edu.vn>
2024-11-21 11:39:56 +0700

    fix: correct some typo missing

--- src/Snake.h
+++ src/Snake.h
@@ -14,7 +14,7 @@ public:
     void grow();                                                                    // Tăng chiều dài rắn khi ăn thức ăn
     bool checkCollision(const sf::FloatRect& playArea) const;                       // Kiểm tra va chạm
     bool checkCollisionWithFood(const sf::Vector2f& foodPosition) const;            // Kiểm tra va chạm với thức ăn
-    void draw(sf::RenderWindow &window);                                            // Vẽ rắn lên cửa sổ
+    void draw(sf::RenderWindow &window) const;                                            // Vẽ rắn lên cửa sổ
 
 private:
     std::vector<sf::RectangleShape> body;                                           // Các đoạn của rắn
