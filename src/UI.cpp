6316f05d481554f2f238e37c67421ba6a34294ca
Chung Kiet Lam <153978151+ckietlam@users.noreply.github.com>
2024-11-21 12:54:42 +0700

    fix: reup nền mới, texture rắn mới, cập nhật thêm về giao diện hiển thị giữa các màn (#23)

--- src/UI.cpp
+++ src/UI.cpp
@@ -9,22 +9,22 @@ UI::UI() {
     }
 
     // Load background
-    if (!backgroundTexture.loadFromFile("assets/images/background.png")) {
+    if (!backgroundTexture.loadFromFile("assets/images/background-w.png")) {
         std::cerr << "Failed to load background texture!" << std::endl;
     }
     backgroundSprite.setTexture(backgroundTexture);
 
     // Define PlayArea
-    playArea = sf::FloatRect(0, 105 , 800, 495);
+    playArea = sf::FloatRect(0, 100 ,800 , 820);
 
     // Load textures for Snake Head and Body
-    if (!snakeHeadTexture.loadFromFile("assets/images/head_no_alpha.png")) {
+    if (!snakeHeadTexture.loadFromFile("assets/images/head.png")) {
         std::cerr << "Failed to load head texture!" << std::endl;
     } else {
         std::cout << "Head texture loaded successfully!" << std::endl;
     }
 
-    if (!snakeBodyTexture.loadFromFile("assets/images/body_no_alpha.png")) {
+    if (!snakeBodyTexture.loadFromFile("assets/images/body.png")) {
         std::cerr << "Failed to load body texture!" << std::endl;
     } else {
         std::cout << "Body texture loaded successfully!" << std::endl;
@@ -33,36 +33,43 @@ UI::UI() {
     // Title Text
     titleText.setFont(font);
     titleText.setString("Snake Game");
-    titleText.setCharacterSize(32);
-    titleText.setFillColor(sf::Color::Red);
-    titleText.setPosition(300, 200); // Vị trí thay đổi để cân đối trên nền
+    titleText.setCharacterSize(42);
+    titleText.setFillColor(sf::Color::Black);
+    titleText.setPosition(260, 360); // Vị trí thay đổi để cân đối trên nền
 
     // Score Text
     scoreText.setFont(font);
-    scoreText.setCharacterSize(32);
-    scoreText.setFillColor(sf::Color::Black);
-    scoreText.setPosition(50, 20);
+    scoreText.setCharacterSize(36);
+    scoreText.setFillColor(sf::Color::White);
+    scoreText.setPosition(310, 45);
 
     // Game Over Text
     gameOverText.setFont(font);
     gameOverText.setString("Game Over!");
-    gameOverText.setCharacterSize(32);
-    gameOverText.setFillColor(sf::Color::Red);
-    gameOverText.setPosition(300, 200);
+    gameOverText.setCharacterSize(42);
+    gameOverText.setFillColor(sf::Color::Black);
+    gameOverText.setPosition(270, 360);
+
+    // Credit Text
+    creditText.setFont(font);
+    creditText.setString(L"SS004.P12 - Game thực hiện bởi Nhóm 20");
+    creditText.setCharacterSize(30);
+    creditText.setFillColor(sf::Color::White);
+    creditText.setPosition(80, 50);
 
     // Play Button
     if (!playButtonTexture.loadFromFile("assets/images/play.png")) { // Cập nhật ảnh play.png
         std::cerr << "Failed to load play button texture!" << std::endl;
     }
     playButton.setTexture(playButtonTexture);
-    playButton.setPosition(332, 277); // Căn giữa theo kích thước cửa sổ mới
+    playButton.setPosition(250, 420); // Căn giữa theo kích thước cửa sổ mới
 
     // Replay Button
     if (!replayButtonTexture.loadFromFile("assets/images/replay.png")) {
         std::cerr << "Failed to load replay button texture!" << std::endl;
     }
     replayButton.setTexture(replayButtonTexture);
-    replayButton.setPosition(332, 277); // Giữ vị trí tương tự nút Play
+    replayButton.setPosition(255, 420); // Giữ vị trí tương tự nút Play
 }
 
 void UI::displayStartScreen(sf::RenderWindow& window) {
@@ -70,6 +77,7 @@ void UI::displayStartScreen(sf::RenderWindow& window) {
     window.draw(backgroundSprite);
     window.draw(titleText);
     window.draw(playButton);
+    window.draw(creditText);
     window.display();
 }
 
