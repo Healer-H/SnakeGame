49336775d0586457d2da816ceca67c20cfe00a1b
Minh Hieu Huynh <22520434@gm.uit.edu.vn>
2024-11-23 11:44:49 +0700

    fix: fix snake movement

--- src/Game.h
+++ src/Game.h
@@ -5,29 +5,34 @@
 #include "Snake.h"
 #include "Food.h"
 #include "UI.h"
+#include <chrono>
 
 enum class GameState {
     Start,
     Run,
     End
 };
+
 class Game {
 public:
     Game();
-    void run();  // Chạy vòng lặp chính của trò chơi
+    void run();
 
 private:
+    void processInput();
+    void update();
+    void render();
+
     sf::RenderWindow window;
     Snake snake;
     Food food;
     UI ui;
-
     int score;
     GameState gameState;
 
-    void processInput();  // Xử lý đầu vào của người chơi
-    void update();        // Cập nhật trạng thái trò chơi
-    void render();        // Vẽ các thành phần lên cửa sổ
+    bool canAcceptInput; 
+    std::chrono::milliseconds inputDelay; 
+    std::chrono::time_point<std::chrono::steady_clock> lastInputTime; 
 };
 
-#endif
+#endif // GAME_H
